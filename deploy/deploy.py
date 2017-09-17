13a5cae505677efaceacfb4b5c2d331070c1a1dc
CSester <cyril.sester@outlook.com>
2017-09-17 20:19:37 +0200

    Fix install call

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -2,5 +2,5 @@ from fabric.api import run, env, sudo
 
 def runDeploy():
   print(env)
-  sudo('apt-get install git')
+  sudo('dnf install git -y')
   run('git clone git@github.com:CSester/dummy-repo2.git -b ' + env.source_commit)
